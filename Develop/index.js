4c73c1158817f2dfbd3ccb0bdf7fb6e9b42b4527
Mikeysoar <mwclimb@gmail.com>
2021-06-26 21:34:14 -0600

    added strater code from mudule

---
+++ Develop/index.js
@@ -0,0 +1,169 @@
+const inquirer = require('inquirer');
+// const generatePage = require('./src/page-template');
+// const { writeFile, copyFile } = require('./utils/generate-site.js');
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name? (Required)',
+//       validate: nameInput => {
+//         if (nameInput) {
+//           return true;
+//         } else {
+//           console.log('Please enter your name!');
+//           return false;
+//         }
+//       }      
+//     },
+//     {
+//       type: 'input',
+//       name: 'github',
+//       message: 'Enter your GitHub Username (Required)',
+//       validate: githubInput => {
+//         if (githubInput) {
+//           return true;
+//         } else {
+//           console.log('Please enter your Github Usernmae!');
+//           return false;
+//         }
+//       } 
+//     },
+//     {
+//       type: 'confirm',
+//       name: 'confirmAbout',
+//       message: 'Would you like to enter some information about yourself for an "About" section?',
+//       default: true
+//     },
+//     {
+//       type: 'input',
+//       name: 'about',
+//       message: 'Provide some information about yourself:',
+//       when: ({ confirmAbout }) => {
+//         if (confirmAbout) {
+//           return true;
+//         } else {
+//           return false;
+//         }
+//       }
+//     }
+//   ]);
+// };
+
+// input project title
+// Input description
+// input table of contents
+// installation. what are the steps to install your project
+// usage
+// credits list of collaberators
+//  License lets others know what they can and can't do with your 
+// project
+// badges?
+// features
+// contributing
+// tests go the extra miler and write tests. . The provide an example how to run them
+
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+// If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Project name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log('Please enter your project description!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github link!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # porfessional-readme-generator
+https://github.com/coding-boot-camp/potential-enigma/blob/master/readme-guide.md
